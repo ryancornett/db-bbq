d878137da66af56f2209877da651726022eefe49
Ryan Cornett <ryandcornett@gmail.com>
2022-10-20 18:56:24 -0400

    Canvas, mobile hamburger menu, and accordion boxes started

--- README.md
+++ README.md
@@ -1,8 +1,46 @@
-This website for a fictional BBQ take-out restaurant is my required project submission
-for the fall 2022 Code Kentucky Web Dev Part 1 course.
+# Danny Boone's BBQ -- website by Ryan Cornett  
+## Code Kentucky Web Development pt 1 Fall 2022 Final Project  
 
-"Danny Boone's BBQ" is named after my beloved uncle, Reuben Daniel Cornett
-(affectionately known as "Boone" by many).
+---
 
-Project requirements met include:
---info to be added at a later date--
+"Danny Boone's BBQ" is named after my beloved uncle, Reuben Daniel Cornett (affectionately known as "Boone" by many).  
+I'm passionate about smoking meats and providing good food to family, so I had a lot of fun with this project.  
+
+---
+
+### Project Plan Wireframe:
+
+https://design.penpot.app/#/view/c6d71d80-3e91-11ed-b414-cf2300df1f4f?page-id=c6d71d81-3e91-11ed-b414-cf2300df1f4f&section=interactions&index=0&share-id=1ddc8470-3f2e-11ed-bf98-c67f7655c7b3
+
+---
+
+## Features List:
+ 1. Create a responsive menu with JavaScript
+ 2. Create a custom animation that animates across the screen
+ 3. Create accordion boxes to display different types of information
+ 4. Add your own transition animation(s) to an element on hover  
+
+---
+  
+## Checklist:
+- [x] Project plan features list
+- [x] Project plan wireframe with mobile, tablet, and desktop views
+- [ ] Project brief explaining the project and included features
+---
+- [x] Website visually appealing
+- [x] Color palette
+- [x] Font stack
+- [x] Mobile-first CSS base
+- [ ] Tablet media query
+- [ ] Desktop media query
+- [x] Project uploaded to GitHub repository with at minimum 5 separate commits
+- [x] Project README file (1+ paragraph project description, features included)
+- [x] Headers, footers, and body sections
+- [x] Semantic HTML
+- [ ] Flexbox or grid page layout
+- [ ] Best efforts for accessibility
+---
+- [ ] Pluralsight videos
+- [ ] Informational interview
+- [x] Tech event
+- [ ] Job readiness workshop
--- main.js
+++ main.js
@@ -1,118 +1,90 @@
+// Mobile Hamburger Menu ****************************************************************
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-// Begin slider code
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
-// $('.slider').each(function() {
-//     var $this = $(this);
-//     var $group = $this.find('.slide_group');
-//     var $slides = $this.find('.slide');
-//     var bulletArray = [];
-//     var currentIndex = 0;
-//     var timeout;
-    
-//     function move(newIndex) {
-//       var animateLeft, slideLeft;
-      
-//       advance();
-      
-//       if ($group.is(':animated') || currentIndex === newIndex) {
-//         return;
-//       }
-      
-//       bulletArray[currentIndex].removeClass('active');
-//       bulletArray[newIndex].addClass('active');
-      
-//       if (newIndex > currentIndex) {
-//         slideLeft = '100%';
-//         animateLeft = '-100%';
-//       } else {
-//         slideLeft = '-100%';
-//         animateLeft = '100%';
-//       }
-      
-//       $slides.eq(newIndex).css({
-//         display: 'block',
-//         left: slideLeft
-//       });
-//       $group.animate({
-//         left: animateLeft
-//       }, function() {
-//         $slides.eq(currentIndex).css({
-//           display: 'none'
-//         });
-//         $slides.eq(newIndex).css({
-//           left: 0
-//         });
-//         $group.css({
-//           left: 0
-//         });
-//         currentIndex = newIndex;
-//       });
-//     }
-    
-//     function advance() {
-//       clearTimeout(timeout);
-//       timeout = setTimeout(function() {
-//         if (currentIndex < ($slides.length - 1)) {
-//           move(currentIndex + 1);
-//         } else {
-//           move(0);
-//         }
-//       }, 4000);
-//     }
-    
-//     $('.next_btn').on('click', function() {
-//       if (currentIndex < ($slides.length - 1)) {
-//         move(currentIndex + 1);
-//       } else {
-//         move(0);
-//       }
-//     });
-    
-//     $('.previous_btn').on('click', function() {
-//       if (currentIndex !== 0) {
-//         move(currentIndex - 1);
-//       } else {
-//         move(3);
-//       }
-//     });
-    
-//     $.each($slides, function(index) {
-//       var $button = $('<a class="slide_btn">&bull;</a>');
-      
-//       if (index === currentIndex) {
-//         $button.addClass('active');
-//       }
-//       $button.on('click', function() {
-//         move(index);
-//       }).appendTo('.slide_buttons');
-//       bulletArray.push($button);
-//     });
-    
-//     advance();
-//   });
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
 
-  // End slider code
-  
-  const date = new Date();
-  let currentDate = "date";
-  var minutes = date.getMinutes();
+// Animated Canvas ********************************************************************
 
-  if (date.getHours() > 12 && minutes < 10) {
-      var pm = date.getHours();
-      pm = pm - 12;
-      currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${pm}:0${date.getMinutes()} PM`;
-  } else if (date.getHours() > 12 && minutes > 9) {
-      var pm = date.getHours();
-      pm = pm - 12;
-      currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${pm}:${date.getMinutes()} PM`;
-  } else if (date.getHours() < 13 && minutes < 10) {
-      currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${date.getHours()}:0${date.getMinutes()} AM`;
+const canvas = document.getElementById('canvas');
+            const context = canvas.getContext('2d');
+            let time = 0;
+
+            const color = function (x, y, r, g, b) {
+                context.fillStyle = `rgb(${r}, ${g}, ${b})`
+                context.fillRect(x, y, 10, 10);
+            }
+            const R = function (x, y, time) {
+                return (Math.floor(255 + 64 * Math.cos((x * x - y * y) / 300 + time)));
+            }
+
+            const G = function (x, y, time) {
+                return (Math.floor(80 + 64 * Math.sin((x * x * Math.cos(time / 4) + y * y * Math.sin(time / 3)) / 300)));
+            }
+
+            const B = function (x, y, time) {
+                return (Math.floor(20 + 64 * Math.sin(5 * Math.sin(time / 9) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100)));
+            }
+
+            const startAnimation = function () {
+                for (x = 0; x <= 200; x++) {
+                    for (y = 0; y <= 450; y++) {
+                        color(x, y, R(x, y, -time), G(x, y, time), B(x, y, time));
+                    }
+                }
+                time = time + 0.03;
+                window.requestAnimationFrame(startAnimation);
+            }
+
+            startAnimation();
+
+// Accordion Boxes ****************************************************
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    } 
+    });
+}
+
+// Footer ***************************************
+
+const date = new Date();
+let currentDate = "date";
+var minutes = date.getMinutes();
+
+if (date.getHours() > 12 && minutes < 10) {
+    var pm = date.getHours();
+    pm = pm - 12;
+    currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${pm}:0${date.getMinutes()} PM`;
+} else if (date.getHours() > 12 && minutes > 9) {
+    var pm = date.getHours();
+    pm = pm - 12;
+    currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${pm}:${date.getMinutes()} PM`;
+} else if (date.getHours() < 13 && minutes < 10) {
+    currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${date.getHours()}:0${date.getMinutes()} AM`;
   }
-  else {
-      currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${date.getHours()}:${date.getMinutes()} AM`;
+else {
+    currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${date.getHours()}:${date.getMinutes()} AM`;
   }
-  document.getElementById("date-time").innerHTML = currentDate;
+document.getElementById("date-time").innerHTML = currentDate;
   
-  const footerDate = new Date();
-  document.getElementById("current-year").innerHTML = footerDate.getFullYear();
+const footerDate = new Date();
+document.getElementById("current-year").innerHTML = footerDate.getFullYear();

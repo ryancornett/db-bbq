8165cddf3639cab20777ffe8a4713ac2463822d2
Ryan Cornett <ryandcornett@gmail.com>
2022-10-26 10:08:54 -0400

    Refactored code; started using SASS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Mobile Hamburger Menu ****************************************************************
+// Mobile Hamburger Menu *** (can't find source from which I customized) ***
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
@@ -13,24 +13,33 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }))
 
-// Add .fixed-header-behind to header at scroll position *******************************
+// Add/remove classes based on scroll position
+
+const scrlDef = 
+    ['header',
+    'nav',
+    '.nav-link',
+    '.logo-shift'];
+
+const scrlAlt = 
+    ['fixed-header-behind',
+    'navbar-light',
+    'nav-link-dark',
+    'logo-shift-black'];
 
 $(window).scroll(function(){
     if ($(this).scrollTop() > 500) {
-       $('header').addClass('fixed-header-behind');
-       $('nav').addClass('navbar-light');
-       $('.nav-link').addClass('nav-link-dark');
-       $('.logo-shift').addClass('logo-shift-black');
+        for (let i = 0; i < scrlDef.length; i++) {
+            $(scrlDef[i]).addClass(scrlAlt[i]);
+        }
     } else {
-       $('header').removeClass('fixed-header-behind');
-       $('nav').removeClass('navbar-light');
-       $('.nav-link').removeClass('nav-link-dark');
-       $('.logo-shift').removeClass('logo-shift-black');
-    }
+        for (let i = 0; i < scrlDef.length; i++) {
+            $(scrlDef[i]).removeClass(scrlAlt[i]);
+    }}
 
 });
 
-// Random text injected into canvas ***************************************************
+// Random text canvas display
 
 const animation_Text = 
     ["low and slow, baby",
@@ -40,25 +49,18 @@ const animation_Text =
 
 let a = Math.floor(Math.random() * 4 + 1);
 
-switch (a) {
-  case 1:
-    document.getElementById("random").textContent = animation_Text[0];
-    document.getElementById("random-reflection").textContent = animation_Text[0];
-    break;
-  case 2:
-    document.getElementById("random").textContent = animation_Text[1];
-    document.getElementById("random-reflection").textContent = animation_Text[1];
-    break;
-  case 3:
-    document.getElementById("random").textContent = animation_Text[2];
-    document.getElementById("random-reflection").textContent = animation_Text[2];
-    break;
-  case 4:
-    document.getElementById("random").textContent = animation_Text[3];
-    document.getElementById("random-reflection").textContent = animation_Text[3];
+function displayText() {
+    document.getElementById("random").textContent = animation_Text[a - 1];
+    document.getElementById("random-reflection").textContent = animation_Text[a - 1];
 }
 
-// Animated Canvas ********************************************************************
+for (let j = 0; j < animation_Text.length; j++) {
+    if (j == a) {
+        displayText();
+    } else {}
+};
+
+// Animated canvas customized from: https://dev.to/jordienr/how-to-make-animated-gradients-like-stripe-56nh
 
 const canvas = document.getElementById('canvas');
             const context = canvas.getContext('2d');
@@ -92,15 +94,15 @@ const canvas = document.getElementById('canvas');
 
             startAnimation();
 
-// Accordion Boxes ****************************************************
+// Accordion Boxes customized from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible_symbol
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+let coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.maxHeight){
         content.style.maxHeight = null;
     } else {
@@ -109,27 +111,26 @@ for (i = 0; i < coll.length; i++) {
     });
 }
 
-// Footer ***************************************
+// Footer
 
 const date = new Date();
 let currentDate = "date";
-var minutes = date.getMinutes();
+let minutes = date.getMinutes();
 
 if (date.getHours() > 12 && minutes < 10) {
-    var pm = date.getHours();
+    let pm = date.getHours();
     pm = pm - 12;
     currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${pm}:0${date.getMinutes()} PM`;
 } else if (date.getHours() > 12 && minutes > 9) {
-    var pm = date.getHours();
+    let pm = date.getHours();
     pm = pm - 12;
     currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${pm}:${date.getMinutes()} PM`;
 } else if (date.getHours() < 13 && minutes < 10) {
     currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${date.getHours()}:0${date.getMinutes()} AM`;
-  }
-else {
+} else {
     currentDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear().toString().substring(2)} ${date.getHours()}:${date.getMinutes()} AM`;
-  }
-document.getElementById("date-time").innerHTML = currentDate;
+}
+document.getElementById("date-time").textContent = currentDate;
   
 const footerDate = new Date();
-document.getElementById("current-year").innerHTML = footerDate.getFullYear();
+document.getElementById("current-year").textContent = footerDate.getFullYear();
